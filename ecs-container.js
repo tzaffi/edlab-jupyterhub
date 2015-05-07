6ce1fc2d5e6df640e2bba8ea3359bb0a8ebb8597
Zeph Grunschlag <zgrunschlag@gmail.com>
2015-05-07 14:39:59 -0400

    Update ecs-container.js

--- ecs-container.js
+++ ecs-container.js
@@ -1,1 +1,37 @@
-TBD
+{
+  "family": "edlab-jupyterhub",
+  "containerDefinitions": [
+    {
+      "name": "edlab-jupyterhub",
+      "image": "edlab-jupyterhub-dev",
+      "cpu": "512",
+      "memory": "2048",
+      "entryPoint": [],
+      "environment": [],
+      "command": [
+        "ipython notebook --ip=0.0.0.0 --no-browser"
+      ],
+      "portMappings": [
+        {
+          "hostPort": "80",
+          "containerPort": "80"
+        }
+      ],
+      "links": [],
+      "mountPoints": [
+        {
+          "sourceVolume": "global-notebooks",
+          "containerPath": "/opt/shared_nbs",
+          "readOnly": false
+        }
+      ],
+      "essential": true
+    }
+  ],
+  "volumes": [
+    {
+      "name": "global-notebooks",
+      "host": {}
+    }
+  ]
+}
